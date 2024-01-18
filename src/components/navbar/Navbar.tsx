5984d9a90393994e49245f6bc7725057f23a92fa
status <Status>
2024-01-18 19:08:40 -0300

    Cadastro Usuario Login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,39 @@
-import { Link } from "react-router-dom"
+﻿import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-  return (
-    <>
-      <div className='w-full bg-indigo-900 text-white
+
+    const navigate = useNavigate();
+
+    const { handleLogout} = useContext(AuthContext)
+
+    function logout(){
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-        <div className="container flex justify-between text-lg">
-          <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-
-          <div className='flex gap-4'>
-            <div className='hover:underline'>Postagens</div>
-            <div className='hover:underline'>Temas</div>
-            <div className='hover:underline'>Cadastrar tema</div>
-            <div className='hover:underline'>Perfil</div>
-            <div className='hover:underline'>Sair</div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
