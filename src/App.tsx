8b64b4ccce1aa0f33e9b721e0838490c571eb311
status <Status>
2024-01-30 15:22:13 -0300

    Alertas Personalizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import './App.css';
-
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import Cadastro from './pages/cadastro/Cadastro';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
+
 import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
 
@@ -19,11 +20,13 @@ import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPosta
 
 
 function App() {
+  
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
+      <ToastContainer />
         <BrowserRouter>
-          <Navbar />
+        <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
@@ -43,8 +46,9 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
   );
 }
+
 export default App;
